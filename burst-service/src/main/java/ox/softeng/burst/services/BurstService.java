85dd04aa68f9a9f1d22925576197b167ae2a90d7
Ollie Freeman <oliver.freeman@ndm.ox.ac.uk>
2016-05-04 08:12:57 +0100

    Add proper commandline options to burst service
    Also rejig the startup process slightly

--- burst-service/src/main/java/ox/softeng/burst/services/BurstService.java
+++ burst-service/src/main/java/ox/softeng/burst/services/BurstService.java
@@ -1,13 +1,12 @@
 package ox.softeng.burst.services;
 
+import org.apache.commons.cli.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 import java.util.concurrent.Executors;
@@ -21,6 +20,10 @@ public class BurstService {
     public static final TimeUnit SCHEDULE_FREQUENCY_UNITS = TimeUnit.MINUTES;
     public static final Integer THREAD_POOL_SIZE = 2;
     private static final Logger logger = LoggerFactory.getLogger(BurstService.class);
+    private static final CommandLineParser parser = new DefaultParser();
+    ScheduledExecutorService executor;
+    Runnable rabbitReceiver;
+    ReportScheduler reportScheduler;
 
     public BurstService(Properties properties) {
 
@@ -39,7 +42,6 @@ public class BurstService {
                                 ).collect(Collectors.toList()));
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("ox.softeng.burst", properties);
 
-
         String RabbitMQHost = properties.getProperty("RabbitMQHost");
         String RabbitMQExchange = properties.getProperty("RabbitMQExchange");
         String RabbitMQQueue = properties.getProperty("RabbitMQQueue");
@@ -48,42 +50,82 @@ public class BurstService {
                     "  host: {}" +
                     "  exchange: {}" +
                     "  queue: {}", RabbitMQHost, RabbitMQExchange, RabbitMQQueue);
-        Runnable rabbitReceiver = new RabbitService(RabbitMQHost, RabbitMQExchange, RabbitMQQueue, entityManagerFactory);
+        rabbitReceiver = new RabbitService(RabbitMQHost, RabbitMQExchange, RabbitMQQueue, entityManagerFactory);
 
-        logger.info("Creating new Report Scheduler");
-        ReportScheduler reportScheduler = new ReportScheduler(entityManagerFactory, properties);
+        logger.info("Creating new report scheduler");
+        reportScheduler = new ReportScheduler(entityManagerFactory, properties);
 
-        logger.info("Creating new executor with report schedule frequency of {} {}",
-                    SCHEDULE_FREQUENCY, SCHEDULE_FREQUENCY_UNITS.name().toLowerCase());
-        ScheduledExecutorService executor = Executors.newScheduledThreadPool(THREAD_POOL_SIZE);
+        logger.info("Creating new executor with thread pool size {}", THREAD_POOL_SIZE);
+        executor = Executors.newScheduledThreadPool(THREAD_POOL_SIZE);
+    }
+
+    public void startService() {
+        logger.info("Starting service with report schedule frequency of {} {}", SCHEDULE_FREQUENCY, SCHEDULE_FREQUENCY_UNITS.name().toLowerCase());
         executor.execute(rabbitReceiver);
         executor.scheduleAtFixedRate(reportScheduler, 0, SCHEDULE_FREQUENCY, SCHEDULE_FREQUENCY_UNITS);
+    }
+
+    private static Options defineOptions() {
+
+        Options options = new Options();
+        OptionGroup group = new OptionGroup();
+        group.addOption(
+                Option.builder("c").longOpt("config")
+                        .argName("FILE")
+                        .hasArg().required()
+                        .desc("The config file defining the monitor config")
+                        .build());
+        group.addOption(Option.builder("h").longOpt("help").build());
+        group.addOption(Option.builder("v").longOpt("version").build());
+        options.addOptionGroup(group);
+
+        return options;
+    }
+
+    private static void help() {
+        HelpFormatter formatter = new HelpFormatter();
+
+        String header = "Monitor RabbitMQ queue for messages. \n" +
+                        "Save them to the database and generate reporting emails based on subscriptions.\n\n";
+        String footer = "\n" + version() + "\n\nPlease report issues at https://github.com/oxbrcinformatics/BuRST/issues\n";
+
+        formatter.printHelp(120,
+                            "burst-service -c <FILE>",
+                            header, defineOptions(), footer, false);
+    }
+
+    private static String version() {
+        return "burst-service version: \"" + BurstService.class.getPackage().getSpecificationVersion() + "\"\n" +
+               "Java Version: \"" + System.getProperty("java.version") + "\"";
 
     }
 
-    public static void main(String[] args) throws FileNotFoundException, IOException {
-        // Arguments:
-        // rabbitmq host name
-        // rabbitmq exchange name
-        // rabbitmq queue name
-        // database server
-        // database db name
-        // database username
-        // database password
+    public static void main(String[] args) throws IOException {
+        try {
+            // parse the command line arguments
+            CommandLine line = parser.parse(defineOptions(), args);
+            if (line.hasOption("h")) help();
+            else if (line.hasOption("v")) System.out.println(version());
+            else if (line.hasOption("c")) {
+                long start = System.currentTimeMillis();
+                try {
 
-        if (args.length < 1) {
-            System.err.println("Usage: BurstService config.properties");
-            System.exit(0);
+                    Properties properties = new Properties();
+                    properties.load(new FileInputStream(line.getOptionValue('c')));
+
+                    BurstService service = new BurstService(properties);
+                    service.startService();
+
+                    logger.info("Burst service started in {}ms", System.currentTimeMillis() - start);
+                } catch (IOException ex) {
+                    logger.error("Could not create burst service because: " + ex.getMessage(), ex);
+                }
+            } else {
+                help();
+            }
+        } catch (ParseException exp) {
+            logger.error("Could not start burst-service because of ParseException: " + exp.getMessage());
+            help();
         }
-        long start = System.currentTimeMillis();
-
-        Properties properties = new Properties();
-        properties.load(new FileInputStream(new File(args[0])));
-
-        new BurstService(properties);
-
-        logger.info("Burst Service started in {}ms", System.currentTimeMillis() - start);
     }
-
-
 }
