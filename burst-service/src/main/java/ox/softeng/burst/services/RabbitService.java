b99b79da21b65b1b6be297c3f870117e4a35a3a7
Ollie Freeman <oliver.freeman@ndm.ox.ac.uk>
2016-05-04 08:15:02 +0100

    Add logging and rework the setup of the rabbitservice
    Logging added to report scheduler
    
    Also split out some of the methods

--- burst-service/src/main/java/ox/softeng/burst/services/RabbitService.java
+++ burst-service/src/main/java/ox/softeng/burst/services/RabbitService.java
@@ -4,81 +4,92 @@ import ox.softeng.burst.domain.Message;
 import ox.softeng.burst.xml.MessageDTO;
 
 import com.rabbitmq.client.*;
+import org.hibernate.HibernateException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
-import javax.xml.bind.JAXB;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
 import java.io.IOException;
-import java.io.StringReader;
 import java.util.concurrent.TimeoutException;
 
 public class RabbitService implements Runnable {
 
-	Channel channel;
-	Connection connection;
-	EntityManagerFactory entityManagerFactory;
-	String rabbitMQQueue;
+    private static final Logger logger = LoggerFactory.getLogger(RabbitService.class);
+    private final EntityManagerFactory entityManagerFactory;
+    private Channel channel;
+    private String rabbitMQQueue;
+    private Unmarshaller unmarshaller;
 
-	public RabbitService(String RabbitMQHost, String RabbitMQExchange, String RabbitMQQueue, EntityManagerFactory emf)
-	{
-		ConnectionFactory factory = new ConnectionFactory();
-		factory.setHost(RabbitMQHost);
-		try {
-			connection = factory.newConnection();
-			channel = connection.createChannel();
-			channel.exchangeDeclare(RabbitMQExchange, "topic", true);
-			this.rabbitMQQueue = RabbitMQQueue;
-			entityManagerFactory = emf;
+    public RabbitService(String rabbitMQHost, String rabbitMQExchange, String rabbitMQQueue, EntityManagerFactory emf) {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(rabbitMQHost);
+        entityManagerFactory = emf;
+        this.rabbitMQQueue = rabbitMQQueue;
 
-			//entityManager.getTransaction().begin();
-			//entityManager.getTransaction().commit();
-		} catch (IOException | TimeoutException e) {
-			e.printStackTrace();
-		}
+        try {
+            Connection connection = factory.newConnection();
+            channel = connection.createChannel();
+            channel.exchangeDeclare(rabbitMQExchange, "topic", true);
+        } catch (IOException | TimeoutException e) {
+            logger.error("Cannot create RabbitMQ  service: " + e.getMessage(), e);
+        }
 
+        try {
+            unmarshaller = JAXBContext.newInstance(MessageDTO.class).createUnmarshaller();
+        } catch (JAXBException e) {
+            logger.error("Cannot create JAXB unmarshaller for messages: " + e.getMessage(), e);
+        }
+    }
 
-	}
+    public void run() {
 
-	public void run() {
+        Consumer consumer = createConsumer();
 
-		EntityManager entityManager = entityManagerFactory.createEntityManager();
+        try {
+            logger.warn("Waiting for messages. To exit press CTRL+C");
+            channel.basicConsume(rabbitMQQueue, true, consumer);
+        } catch (IOException e) {
+            logger.error("Error running consumer for queue '" + rabbitMQQueue + "'", e);
+        } catch (Exception e) {
+            logger.error("Unhandled exception: " + e.getMessage(), e);
+        }
+    }
 
-		System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+    private Consumer createConsumer() {
+        return new DefaultConsumer(channel) {
+            @Override
+            public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
+                String messageString = new String(body, "UTF-8");
+                String tag = properties.getMessageId() != null ? properties.getMessageId() : consumerTag;
+                try {
+                    MessageDTO messageDto = (MessageDTO) unmarshaller.unmarshal(new StreamSource(messageString));
+                    Message m = messageDto.generateMessage();
 
-		Consumer consumer = new DefaultConsumer(channel) {
-			@Override
-			public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
-					throws IOException {
-				String messageString = new String(body, "UTF-8");
-				MessageDTO messageDto = JAXB.unmarshal(new StringReader(messageString), MessageDTO.class);
-				Message m = messageDto.generateMessage();
+                    logger.debug("{} - Received message DTO: \n{}", tag, messageDto.toString());
 
-
-				System.out.println("Received message metadata size: " + messageDto.getMetadata().size());
-				System.out.println("Stored message metadata size: " + m.getMetadata().size());
-
-				entityManager.getTransaction().begin();
-				entityManager.merge(m);
-				//for(Metadata md : m.getMetadata())
-				//{
-				//	entityManager.merge(md);
-				//}
-				entityManager.getTransaction().commit();
-
-				System.out.println(" [x] Received '" + messageDto.toString() + "'");
-
-
-			}
-		};
-		try {
-			channel.basicConsume(rabbitMQQueue, true, consumer);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		//entityManagerFactory.close();
-		//entityManager.close();
-	}
+                    logger.debug("{} - Saving message to database", tag);
+                    EntityManager entityManager = entityManagerFactory.createEntityManager();
+                    entityManager.getTransaction().begin();
+                    entityManager.merge(m);
+                    entityManager.getTransaction().commit();
+                    entityManager.close();
+                    logger.debug("{} - Message saved", tag);
+                    logger.info("{} - Received {} message from '{}'", tag, m.getSeverity(), m.getSource());
+                } catch (JAXBException e) {
+                    logger.error(tag + " - Could not unmarshall message\n" + messageString, e);
+                } catch (HibernateException he) {
+                    logger.error(tag + " - Could not save message to database: " + he.getMessage(), he);
+                } catch (Exception e) {
+                    logger.error(tag + " - Unhandled exception trying to process message", e);
+                }
+            }
+        };
+    }
 
 
 }
--- burst-service/src/main/java/ox/softeng/burst/services/ReportScheduler.java
+++ burst-service/src/main/java/ox/softeng/burst/services/ReportScheduler.java
@@ -1,8 +1,10 @@
 package ox.softeng.burst.services;
 
-import java.time.OffsetDateTime;
-import java.util.List;
-import java.util.Properties;
+import ox.softeng.burst.domain.Severity;
+import ox.softeng.burst.domain.Subscription;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import javax.mail.Message;
 import javax.mail.MessagingException;
@@ -13,193 +15,184 @@ import javax.mail.internet.MimeMessage;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.TypedQuery;
+import java.time.OffsetDateTime;
+import java.util.List;
+import java.util.Properties;
 
-import ox.softeng.burst.domain.Frequency;
-import ox.softeng.burst.domain.Severity;
-import ox.softeng.burst.domain.Subscription;
-import ox.softeng.burst.domain.User;
+public class ReportScheduler implements Runnable {
 
-public class ReportScheduler implements Runnable{
+    private static final Logger logger = LoggerFactory.getLogger(ReportScheduler.class);
+    private final EntityManagerFactory entityManagerFactory;
+    private String emailsFrom;
+    private String smtpHost;
+    private String smtpPassword;
+    private String smtpUsername;
 
-	EntityManagerFactory entityManagerFactory;
-	String emailsFrom; 
-	String smtpHost;
-	String smtpUsername;
-	String smtpPassword;
+    public ReportScheduler(EntityManagerFactory emf, Properties props) {
+        entityManagerFactory = emf;
+        emailsFrom = props.getProperty("smtp-from");
+        smtpHost = props.getProperty("smtp-host");
+        smtpUsername = props.getProperty("smtp-username");
+        smtpPassword = props.getProperty("smtp-password");
+    }
 
-	
-	
-	public ReportScheduler(EntityManagerFactory emf, Properties props)
-	{
-		this.entityManagerFactory = emf;
-		emailsFrom = props.getProperty("smtp-from");
-		smtpHost = props.getProperty("smtp-host");
-		smtpUsername = props.getProperty("smtp-username");
-		smtpPassword = props.getProperty("smtp-password");
-	}
-	
-	@Override
-	public void run() {
-		//System.out.println("Running version 1.1.2-SNAPSHOT...");
-		System.out.println("Generating reports...");
+    @Override
+    public void run() {
 
-		OffsetDateTime now = OffsetDateTime.now();
-		
-		
-		// First we find all the subscriptions where the "next time" is less than now
-		
-		EntityManager em = entityManagerFactory.createEntityManager();
-		TypedQuery<Subscription> subsQuery = em.createNamedQuery("Subscription.allDue", Subscription.class);
-		subsQuery.setParameter("dateNow", now);
-		List<Subscription> dueSubscriptions = subsQuery.getResultList();
-		
-		System.out.println("no. of active subscriptions: " + dueSubscriptions.size());
-		for(Subscription s : dueSubscriptions)
-		{
-			System.out.println("Calculating number of topics...");
-			if(s.getTopics().size() == 0)
-			{
-				System.out.println("Subscription " + s.getId() + " has no registered topics!");
-			}
-			else
-			{
-				// For each of those, we find all the matching messages
-				Severity severity = s.getSeverity();
-				//Frequency frequency = s.getFrequency();
-				User u = s.getSubscriber();
-				System.out.println("user email:" + u.getEmailAddress());
-				
-				EntityManager msgEm = entityManagerFactory.createEntityManager();
-				System.out.println("Created entity manager");
-				TypedQuery<ox.softeng.burst.domain.Message> msgQuery = msgEm.createNamedQuery("Message.MatchedMessages", ox.softeng.burst.domain.Message.class);
-				
-				System.out.println("Setting date now " + now.toString());
-				msgQuery.setParameter("dateNow", now);
-				System.out.println("Setting last run date " + s.getLastScheduledRun().toString());
-				msgQuery.setParameter("lastSentDate", s.getLastScheduledRun());
-				System.out.println("Setting severity: " + severity.getSeverity().ordinal());
-				msgQuery.setParameter("severity", severity.getSeverity().ordinal());
-				//msgQuery.setParameter("topics", s.getTopics());
-				//msgQuery.setParameter("topicsSize", s.getTopics().size());
-				List<ox.softeng.burst.domain.Message> matchedMessages = msgQuery.getResultList();
-				System.out.println("Retrieved matched messages");
-				System.out.println("Size: " + matchedMessages.size());
-				System.out.println("matched messages: " + matchedMessages.size());
-	
-				
-				String emailContent = "";
-				int count = 0;
-				for(ox.softeng.burst.domain.Message msg : matchedMessages)
-				{
-					System.out.println("message topics size: " + msg.getTopics().size());
-					System.out.println("subscription topics size: " + s.getTopics().size());
-					// Now we ensure the subscribed topics are a subset of the message topics
-					// Can't get this working within the query itself
-					if(msg.getTopics().containsAll(s.getTopics()))
-					{
-						// We send a message with the concatenation of all the messages
-						emailContent += msg.getMessage();
-						emailContent += "\n\n";
-						count ++;
-					}					
-				}
-				System.out.println("topic matched messages: " + matchedMessages.size());
-	
-				if(count > 0)
-				{
-					System.out.println("Sending an email to: " + s.getSubscriber().getEmailAddress());
-					System.out.println("Content: ");
-					System.out.println(emailContent);
-					sendMessage(s.getSubscriber().getEmailAddress(),  "Genomics England reporting message", emailContent);
-				}
-				
-				// Then we re-calculate the "last sent" and "next send" timestamps and update the record 
-				s.setLastScheduledRun(now);
-				s.calculateNextScheduledRun();
-				EntityManager subEm = entityManagerFactory.createEntityManager();
-				subEm.getTransaction().begin();
-				subEm.merge(s);
-				subEm.getTransaction().commit();
-				
-				subEm.close();
-				msgEm.close();
-			}			
-		}
-		em.close();
+        try {
+            logger.info("Generating reports");
 
-		
-		
-		// Finally we find any subscription where the "time of next run" is not set,
-		// and put a time on it.
-		
-		EntityManager badSubsEm = entityManagerFactory.createEntityManager();
-		TypedQuery<Subscription> badSubsQuery = badSubsEm.createNamedQuery("Subscription.unInitialised", Subscription.class);
-		List<Subscription> uninitialisedSubscriptions = badSubsQuery.getResultList();
-		System.out.println("Uninitialised subscriptions: " + uninitialisedSubscriptions.size()) ;
-		badSubsEm.close();
-		for(Subscription s : uninitialisedSubscriptions )
-		{
-			EntityManager schedEm = entityManagerFactory.createEntityManager();
-			schedEm.getTransaction().begin();
-			s.calculateNextScheduledRun();
-			schedEm.merge(s);
-			schedEm.getTransaction().commit();
-			schedEm.close();
-		}
+            OffsetDateTime now = OffsetDateTime.now();
 
-	}
+            // First we find all the subscriptions where the "next time" is less than now
+            EntityManager em = entityManagerFactory.createEntityManager();
+            TypedQuery<Subscription> subsQuery = em.createNamedQuery("Subscription.allDue", Subscription.class);
+            subsQuery.setParameter("dateNow", now);
+            List<Subscription> dueSubscriptions = subsQuery.getResultList();
 
+            logger.info("Handling {} active subscriptions", dueSubscriptions.size());
+            for (Subscription s : dueSubscriptions) {
+                logger.debug("Calculating number of topics");
 
-	private void sendMessage(String emailAddress, String subject, String contents)
-	{
-		// Get system properties
-		Properties properties = System.getProperties();
-		// Setup mail server
-		properties.setProperty("mail.smtp.host", smtpHost);
-		// Get the default Session object.
-		Session session = Session.getDefaultInstance(properties);
-		String protocol = "smtp";
-		if(smtpUsername == null || "".equals(smtpUsername))
-		{
-			System.out.println("sending without authentication...");
-			properties.put("mail." + protocol + ".auth", "false");
-		}
-		else{
-			System.out.println("setting authentication method...");
-			properties.put("mail." + protocol + ".auth", "true");
-			properties.put("mail.smtp.starttls.enable", "true");
-		}
-		try{
-			Transport t = session.getTransport(protocol);
-			// Create a default MimeMessage object.
-			MimeMessage message = new MimeMessage(session);
-			// Set From: header field of the header.
-			message.setFrom(new InternetAddress(emailsFrom));
-			// Set To: header field of the header.
-			message.addRecipient(Message.RecipientType.TO, new InternetAddress(emailAddress));
-			// Set Subject: header field
-			message.setSubject(subject);
-			// Now set the actual message contents
-			message.setText(contents);
-			// Send message
-		    try {
-				if(smtpUsername == null || "".equals(smtpUsername))
-				{
-					t.connect();
-				}
-				else{
-					t.connect(smtpUsername, smtpPassword);
-				}
-		        t.sendMessage(message, message.getAllRecipients());
-		    } finally {
-		        t.close();
-		    }
-			System.out.println("Sent message successfully....");
-		}catch (MessagingException mex) {
-			mex.printStackTrace();
-		}
+                if (s.getTopics().size() == 0) {
+                    logger.warn("Subscription {} has no registered topics", s.getId());
+                } else {
+                    logger.debug("Handling subscription: {}", s.getId());
 
-	}
+                    // For each of those, we find all the matching messages
+                    List<ox.softeng.burst.domain.Message> matchedMessages = findMessagesForSubscription(s, now, s.getSeverity());
 
+                    String emailContent = "";
+                    int count = 0;
+                    logger.debug("Generating email content for {} subscription topics", s.getTopics().size());
 
+                    for (ox.softeng.burst.domain.Message msg : matchedMessages) {
+                        logger.debug("Checking message with {} topics", msg.getTopics().size());
+                        // Now we ensure the subscribed topics are a subset of the message topics
+                        // Can't get this working within the query itself
+                        if (msg.getTopics().containsAll(s.getTopics())) {
+                            // We send a message with the concatenation of all the messages
+                            emailContent += msg.getMessage();
+                            emailContent += "\n\n";
+                            count++;
+                        }
+                    }
+                    logger.debug("Email generated for {} messages", count);
+
+                    if (count > 0) {
+                        logger.info("Sending an email to: " + s.getSubscriber().getEmailAddress());
+                        logger.debug("Content: \n{}", emailContent);
+                        sendMessage(s.getSubscriber().getEmailAddress(), "Genomics England reporting message", emailContent);
+                    }
+
+                    // Then we re-calculate the "last sent" and "next send" timestamps and update the record
+                    updateSubscription(s, now);
+                }
+            }
+            em.close();
+
+            // Finally we find any subscription where the "time of next run" is not set,
+            // and put a time on it.
+            initialiseSubscriptions();
+
+            logger.info("Reports generated");
+        } catch (Exception ex) {
+            logger.error("Unhandled exception: " + ex.getMessage(), ex);
+        }
+    }
+
+    private List<ox.softeng.burst.domain.Message> findMessagesForSubscription(Subscription subscription, OffsetDateTime runTime, Severity severity) {
+        EntityManager msgEm = entityManagerFactory.createEntityManager();
+
+        logger.debug("Creating message query with [Date '{}', Last run Date '{}', Severity '{}']",
+                     runTime, subscription.getLastScheduledRun(), severity.getSeverity());
+        TypedQuery<ox.softeng.burst.domain.Message> msgQuery = msgEm.createNamedQuery("Message.MatchedMessages",
+                                                                                      ox.softeng.burst.domain.Message.class);
+        msgQuery.setParameter("dateNow", runTime);
+        msgQuery.setParameter("lastSentDate", subscription.getLastScheduledRun());
+        msgQuery.setParameter("severity", severity.getSeverity().ordinal());
+        //msgQuery.setParameter("topics", s.getTopics());
+        //msgQuery.setParameter("topicsSize", s.getTopics().size());
+        logger.debug("Getting matching messages");
+        List<ox.softeng.burst.domain.Message> matchedMessages = msgQuery.getResultList();
+        logger.info("Subscription has {} currently matching messages", matchedMessages.size());
+        msgEm.close();
+
+        return matchedMessages;
+    }
+
+    private void initialiseSubscriptions() {
+        EntityManager badSubsEm = entityManagerFactory.createEntityManager();
+        TypedQuery<Subscription> badSubsQuery = badSubsEm.createNamedQuery("Subscription.unInitialised", Subscription.class);
+        List<Subscription> uninitialisedSubscriptions = badSubsQuery.getResultList();
+        logger.debug("Initialising {} subscriptions", uninitialisedSubscriptions.size());
+        badSubsEm.close();
+        for (Subscription s : uninitialisedSubscriptions) {
+            logger.debug("Scheduling new run for {}", s.getId());
+            EntityManager schedEm = entityManagerFactory.createEntityManager();
+            schedEm.getTransaction().begin();
+            s.calculateNextScheduledRun();
+            schedEm.merge(s);
+            schedEm.getTransaction().commit();
+            schedEm.close();
+        }
+    }
+
+    private void sendMessage(String emailAddress, String subject, String contents) {
+        // Get system properties
+        Properties properties = System.getProperties();
+        // Setup mail server
+        properties.setProperty("mail.smtp.host", smtpHost);
+        // Get the default Session object.
+        Session session = Session.getDefaultInstance(properties);
+        String protocol = "smtp";
+        if (smtpUsername == null || "".equals(smtpUsername)) {
+            logger.debug("Sending without authentication");
+            properties.put("mail." + protocol + ".auth", "false");
+        } else {
+            logger.debug("Setting authentication method");
+            properties.put("mail." + protocol + ".auth", "true");
+            properties.put("mail.smtp.starttls.enable", "true");
+        }
+        Transport t = null;
+        try {
+            t = session.getTransport(protocol);
+            // Create a default MimeMessage object.
+            MimeMessage message = new MimeMessage(session);
+            // Set From: header field of the header.
+            message.setFrom(new InternetAddress(emailsFrom));
+            // Set To: header field of the header.
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(emailAddress));
+            // Set Subject: header field
+            message.setSubject(subject);
+            // Now set the actual message contents
+            message.setText(contents);
+            // Send message
+            if (smtpUsername == null || "".equals(smtpUsername)) {
+                t.connect();
+            } else {
+                t.connect(smtpUsername, smtpPassword);
+            }
+            t.sendMessage(message, message.getAllRecipients());
+
+            logger.info("Sent message successfully");
+        } catch (MessagingException mex) {
+            logger.error("Error whilst sending email: " + mex.getMessage(), mex);
+        } finally {
+            try {
+                t.close();
+            } catch (NullPointerException | MessagingException ignored) {}
+        }
+    }
+
+    private void updateSubscription(Subscription subscription, OffsetDateTime lastRun) {
+        logger.debug("Updating last run time and scheduling next run for {}", subscription.getId());
+        subscription.setLastScheduledRun(lastRun);
+        subscription.calculateNextScheduledRun();
+        EntityManager subEm = entityManagerFactory.createEntityManager();
+        subEm.getTransaction().begin();
+        subEm.merge(subscription);
+        subEm.getTransaction().commit();
+        subEm.close();
+    }
 }
