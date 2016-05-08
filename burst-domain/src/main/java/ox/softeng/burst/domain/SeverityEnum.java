d7e3060597fb6a5039e0c1b3ad30526ec3a99a56
Ollie Freeman <oliver.freeman@ndm.ox.ac.uk>
2016-05-08 15:21:38 +0100

    Change the email structure slightly
    Group messages by severity and place worst ones first
    Number the messages grouped by serverity in the subject if default message subject used

--- burst-domain/src/main/java/ox/softeng/burst/domain/SeverityEnum.java
+++ burst-domain/src/main/java/ox/softeng/burst/domain/SeverityEnum.java
@@ -3,6 +3,13 @@ package ox.softeng.burst.domain;
 // Taken from:
 // https://logging.apache.org/log4j/2.0/log4j-core/apidocs/org/apache/logging/log4j/core/net/Severity.html
 
+import org.apache.commons.lang3.text.WordUtils;
+
 public enum SeverityEnum {
-    DEBUG, INFORMATIONAL, NOTICE, WARNING, ERROR, CRITICAL, ALERT, EMERGENCY
+    DEBUG, INFORMATIONAL, NOTICE, WARNING, ERROR, CRITICAL, ALERT, EMERGENCY;
+
+    @Override
+    public String toString() {
+        return WordUtils.capitalizeFully(name());
+    }
 }
